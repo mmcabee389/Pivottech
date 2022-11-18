fe70295c63bc63b74a155fca0ceeb24b2fe2656e
mmcabee389 <105619090+mmcabee389@users.noreply.github.com>
2022-11-17 21:15:38 -0600

    added table driven test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,53 +6,122 @@ import (
 )
 
 func TestAddition(t *testing.T) {
+	cases := []struct {
+		problem string
+		num1    int
+		num2    int
+		answer  int
+	}{
+		{
+			problem: "1+2",
+			num1:    1,
+			num2:    2,
+			answer:  3,
+		},
+		{
+			problem: "4+2",
+			num1:    4,
+			num2:    2,
+			answer:  6,
+		},
+	}
+	for _, ta := range cases {
+		t.Run(ta.problem, func(t *testing.T) {
+			result := calculator.Addition(ta.num1, ta.num2)
+			if result != ta.answer {
+				t.Errorf("expected %d, but got %d", ta.answer, result)
 
-	a, b := 1, 2
-	want := 3
-
-	got := calculator.Addition(a, b)
-
-	if got != want {
-
-		t.Errorf("got %d, want %d", got, want)
+			}
+		})
 	}
 }
 
 func TestSubtraction(t *testing.T) {
-
-	a, b := 5, 3
-	want := 2
-
-	got := calculator.Subtraction(a, b)
-
-	if got != want {
-
-		t.Errorf("got %d, want %d", got, want)
+	cases := []struct {
+		problem string
+		num1    int
+		num2    int
+		answer  int
+	}{
+		{
+			problem: "5-2",
+			num1:    5,
+			num2:    2,
+			answer:  3,
+		},
+		{
+			problem: "4-2",
+			num1:    4,
+			num2:    2,
+			answer:  2,
+		},
+	}
+	for _, ts := range cases {
+		t.Run(ts.problem, func(t *testing.T) {
+			result := calculator.Subtraction(ts.num1, ts.num2)
+			if result != ts.answer {
+				t.Errorf("expected %d, but got %d", ts.answer, result)
+			}
+		})
 	}
 }
 
 func TestDivision(t *testing.T) {
-
-	a, b := 4, 2
-	want := 2
-
-	got, _ := calculator.Division(a, b)
-
-	if got != want {
-
-		t.Errorf("got %d, want %d", got, want)
+	cases := []struct {
+		problem string
+		num1    int
+		num2    int
+		answer  int
+	}{
+		{
+			problem: "8/4",
+			num1:    8,
+			num2:    4,
+			answer:  2,
+		},
+		{
+			problem: "4/2",
+			num1:    4,
+			num2:    2,
+			answer:  2,
+		},
+	}
+	for _, td := range cases {
+		t.Run(td.problem, func(t *testing.T) {
+			result, _ := calculator.Division(td.num1, td.num2)
+			if result != td.answer {
+				t.Errorf("expected %d, but got %d", td.answer, result)
+			}
+		})
 	}
 }
-
 func TestMultiply(t *testing.T) {
+	cases := []struct {
+		problem string
+		num1    int
+		num2    int
+		answer  int
+	}{
+		{
+			problem: "1*2",
+			num1:    1,
+			num2:    2,
+			answer:  2,
+		},
+		{
+			problem: "4*2",
+			num1:    4,
+			num2:    2,
+			answer:  8,
+		},
+	}
+	for _, tm := range cases {
+		t.Run(tm.problem, func(t *testing.T) {
+			result := calculator.Multiply(tm.num1, tm.num2)
+			if result != tm.answer {
+				t.Errorf("expected %d, but got %d", tm.answer, result)
 
-	a, b := 6, 5
-	want := 30
-
-	got := calculator.Multiply(a, b)
-
-	if got != want {
-
-		t.Errorf("got %d, want %d", got, want)
+			}
+		})
 	}
 }
