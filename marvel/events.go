3b8f308b3f7c538105a170269c36235fbe13b4c4
mmcabee389 <105619090+mmcabee389@users.noreply.github.com>
2022-11-25 15:12:02 -0600

    Add files via upload

---
+++ marvel/events.go
@@ -0,0 +1,22 @@
+package main
+
+type Event struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+type EventResponse struct {
+	Code            int    `json:"code"`
+	Status          string `json:"status"`
+	Copyright       string `json:"copyright"`
+	AttributionText string `json:"attributionText"`
+	AttributionHTML string `json:"attributionHTML"`
+	Etag            string `json:"etag"`
+	Data            struct {
+		Offset  int     `json:"offset"`
+		Limit   int     `json:"limit"`
+		Total   int     `json:"total"`
+		Count   int     `json:"count"`
+		Results []Event `json:"results"`
+	} `json:"data"`
+}
